ae21ea21db4112923ab169754e2ac355a41bfb23
오지환 <ojh3212@naver.com>
2023-07-10 16:37:02 +0900

    로그인 페이지 1차 (선재 + 지환)

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,45 +1,152 @@
 import React from "react";
+import styled from "styled-components";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const ServiceName = styled.p`
+    font-size: 3rem;
+    margin: 2rem;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+  `;
+
+  const LoginContainer = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+  `
+
+  const LoginText = styled.p`
+    font-size: 1.5rem;
+    margin: 1rem;
+    font-weight: bold;
+    display: flex;
+    padding-bottom: 1rem;
+  `;
+
+  const LinkDiv = styled.div`
+    /* width: 15rem;
+    height: 2rem; */
+    display: flex;
+    flex-direction: column;
+  `;
+
+  const SearchLink = styled.a`
+    font-size: 1rem;
+    display: flex;
+    justify-content: flex-end;
+    padding-top: 1rem;
+  `;
+
+  const SignupLink = styled.a`
+    font-size: 1rem;
+    display: flex;
+    justify-content: flex-end;
+  `;
+
+  const LoginButtonDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 2rem 0;
+  `;
+
+  const LoginButton = styled.button`
+    background-color: skyblue;
+    width: 17rem;
+    height: 4rem;
+    border-radius: 1rem;
+    border: 0;
+    box-shadow: 0rem 0.3rem 0.3rem gray;
+  `;
+
+  const LoginButtonText = styled.p`
+    font-size: 1.5rem;
+    font-weight: bold;
+    display: flex;
+    margin: 0;
+    justify-content: center;
+  `;
+
+  const SocialLoginText = styled.p`
+    display: flex;
+    justify-content: center;
+  `;
+
+  const SocialLogin = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
 
 const LoginPage = (props) => {
-    const [Id, setID] =useState("") // ID 저장용 useState
-    const [Password, setPassword] = useState("") //Password 저장용 useState
+  const [Id, setID] = useState(""); // ID 저장용 useState
+  const [Password, setPassword] = useState(""); //Password 저장용 useState
 
-    function insertId(e){ // 입력된 ID 받아오는 함수
-        setID(e.target.value)
-    }
+  function insertId(e) {
+    // 입력된 ID 받아오는 함수
+    setID(e.target.value);
+  }
 
-    function insertPassword(e){ // 입력된 Password 받아오는 함수
-        setPassword(e.target.value)
-    }
+  function insertPassword(e) {
+    // 입력된 Password 받아오는 함수
+    setPassword(e.target.value);
+  }
 
-    function BtnClick(){
-        axios
-        .post('http://54.180.85.255/login/',{ // 입력된 userID 와 password 정보를 post로 넘겨주는 코드
-            userID : Id, 
-            password : Password 
-        })
-        .then(() => {
-            console.log(Id) // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (ID check)
-            console.log(Password) // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (Password check)
-            console.log("로그인 성공!")
-        })
-        .catch((e) => { // axios error check하는 코드
-            console.log(e)
-        });
-    }
-    
+  function BtnClick() {
+    axios
+      .post("http://54.180.85.255/signup/", {
+        // 입력된 userID 와 password 정보를 post로 넘겨주는 코드
+        userID: Id,
+        password: Password,
+      })
+      .then(() => {
+        console.log(Id); // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (ID check)
+        console.log(Password); // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (Password check)
+      })
+      .catch((e) => {
+        // axios error check하는 코드
+        console.log(e);
+      });
+  }
+
   return (
     <>
-        {/* ID 입력칸 */}
-        <input onChange={insertId} placehold="ID" value={Id}></input> 
-        
-        {/* Password 입력칸 */}
-        <input onChange={insertPassword} placehold="Password" value={Password}></input>
-        
-        {/* 로그인 버튼 */}
-        <button onClick={BtnClick}>로그인</button>
+      <ServiceName>서비스 이름</ServiceName>
+
+      <LoginContainer>
+      <LoginText Link="">로그인</LoginText>
+      {/* ID 입력칸 */}
+      <input
+        onChange={insertId}
+        placeholder="ID 근데 파란색으로 어케 바꾸냐"
+        value={Id}
+      ></input>
+
+      {/* Password 입력칸 */}
+      <input type="password"
+        onChange={insertPassword}
+        placeholder="Password 근데 파란색으로 어케 바꾸냐"
+        value={Password}
+      ></input>
+      <br></br>
+      </LoginContainer>
+
+      <LinkDiv>
+        <SearchLink>아이디/비밀번호 찾기</SearchLink>
+        <SignupLink>회원가입</SignupLink>
+      </LinkDiv>
+
+      <LoginButtonDiv>
+        <LoginButton>
+          <LoginButtonText onClick={BtnClick}>로그인</LoginButtonText>
+        </LoginButton>
+      </LoginButtonDiv>
+
+      <SocialLoginText>기존 계정으로 로그인</SocialLoginText>
+      <SocialLogin>
+        {" "}
+      </SocialLogin>
     </>
   );
 };
