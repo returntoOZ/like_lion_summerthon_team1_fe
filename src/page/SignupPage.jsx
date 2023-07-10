6d540abcd357cad7115278811795c4af378660d6
sunjae <zxsunje775@naver.com>
2023-07-10 16:46:09 +0900

    SignupPage 미완성

--- src/page/SignupPage.jsx
+++ src/page/SignupPage.jsx
@@ -2,44 +2,109 @@ import React from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const SignupTextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20rem;
+  padding: 4rem 2rem;
+`;
+
+const SignupText = styled.p`
+  font-weight: bold;
+  font-size: 2rem;
+  margin: 0;
+`;
+
+const SignupText2 = styled.p`
+  font-weight: bold;
+  font-size: 0.8rem;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+`;
+
+const NicknameBox = styled.div`
+  width: 18rem;
+`;
+
+const NicknameText = styled.p`
+  font-size: 1rem;
+  color: gray;
+`;
+
+const NicknameInput = styled.input`
+  width: 15rem;
+`;
+
+const IdBox = styled.div`
+  width: 18rem;
+`;
+
+const IdText = styled.p`
+  font-size: 1rem;
+  color: gray;
+`;
+
+const PasswordBox = styled.div`
+  width: 18rem;
+`;
+
+const PasswordText = styled.p`
+  font-size: 1rem;
+  color: gray;
+`;
 
 const SignupPage = (props) => {
-    const [Id, setID] =useState("") // ID 저장용 useState
-    const [Password, setPassword] = useState("") //Password 저장용 useState
+  const [Id, setID] = useState(""); // ID 저장용 useState
+  const [Password, setPassword] = useState(""); //Password 저장용 useState
 
-    function insertId(e){ // 입력된 ID 받아오는 함수
-        setID(e.target.value)
-    }
+  function insertId(e) {
+    // 입력된 ID 받아오는 함수
+    setID(e.target.value);
+  }
 
-    function insertPassword(e){ // 입력된 Password 받아오는 함수
-        setPassword(e.target.value)
-    }
+  function insertPassword(e) {
+    // 입력된 Password 받아오는 함수
+    setPassword(e.target.value);
+  }
 
-    function BtnClick(){
-        axios
-        .post('http://54.180.85.255/signup/',{ // 입력된 userID 와 password 정보를 post로 넘겨주는 코드
-            userID : Id,
-            password : Password
-        })
-        .then(() => {
-            console.log(Id) // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (ID check)
-            console.log(Password) // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (Password check)
-        })
-        .catch((e) => { // axios error check하는 코드
-            console.log(e)
-        });
-    }
-    
+  function BtnClick() {
+    axios
+      .post("http://54.180.85.255/signup/", {
+        // 입력된 userID 와 password 정보를 post로 넘겨주는 코드
+        userID: Id,
+        password: Password,
+      })
+      .then(() => {
+        console.log(Id); // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (ID check)
+        console.log(Password); // 제대로 작동하는 정보 넘겨줬는지 확인하는 코드 (Password check)
+      })
+      .catch((e) => {
+        // axios error check하는 코드
+        console.log(e);
+      });
+  }
+
   return (
     <>
-        {/* ID 입력칸 */}
-        <input onChange={insertId} placehold="ID" value={Id}></input> 
-        
-        {/* Password 입력칸 */}
-        <input onChange={insertPassword} placehold="Password" value={Password}></input>
-        
-        {/* 회원가입 버튼 */}
-        <button onClick={BtnClick}>회원가입</button>
+      <SignupTextBox>
+        <SignupText>회원가입</SignupText>
+        <SignupText2>
+          환영합니다! 회원이 되어 다양한 서비스를 누려보세요
+        </SignupText2>
+      </SignupTextBox>
+
+      <InputBox>
+        <NicknameBox>
+          <NicknameText>닉네임 입력</NicknameText>
+          <NicknameInput></NicknameInput>
+        </NicknameBox>
+        <IdBox></IdBox>
+        <PasswordBox></PasswordBox>
+      </InputBox>
     </>
   );
 };
