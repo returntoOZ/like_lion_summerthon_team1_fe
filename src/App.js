c2a5e9284c244d82c7ef289f2f7e928eeba58352
pakxe <pigkill40@naver.com>
2023-07-13 20:42:48 +0900

    chore: 임시 페이지

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import SearchPage from './page/SearchPage';
 import MyPage from './page/MyPage';
 import CreateRoom from './page/CreateRoom';
 import ChatPage from './page/ChatPage';
+import TempPage from './page/TempPage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<TempPage />} />
           <Route path='/signup' element={<SignupPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/main/:Id1' element={<MainPage />} />
---
+++ src/page/TempPage.jsx
@@ -0,0 +1,3 @@
+export default function TempPage() {
+  return <div>hi</div>;
+}
